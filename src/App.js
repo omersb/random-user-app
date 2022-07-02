11518f58c004ba3c4074aa2fdb20f3de3ea6ae06
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-07-02 17:31:52 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import Card from './Card';
+import "./App.css";
+import Card from "./Card.jsx";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const url = "https://randomuser.me/api/";
+  const [first, setFirst] = useState();
+  const response = async () => {
+    const { data } = await axios.get(url);
+    const response = data.results[0];
+
+    // const {
+    //   cell,
+    //   dob,
+    //   email,
+    //   gender,
+    //   location,
+    //   name,
+    //   picture,
+    //   phone,
+    //   nat,
+    //   registered,
+    // } = response;
+    setFirst(response);
+    console.log(first);
+  };
+  useEffect(() => {
+    response();
+  }, []);
   return (
-    <div className="App">
-      <Card />
+    <div className="container">
+      {first && <Card first={first} />}
+      <button className="btn btn-primary mt-4" onClick={() => response()}>
+        Random User
+      </button>
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,13 +1,69 @@
 import React from "react";
+import emailsvg from "./assets/email.svg";
+import locationsvg from "./assets/location.svg";
+import phonesvg from "./assets/phone.svg";
 
-const Card = () => {
-  const url = "https://randomuser.me/api/";
-  const response = async () => {
-    const rest = await fetch(url).then((x) => x.json());
-    console.log(rest.results[0]);
-  };
-  response();
-  return <div>Card</div>;
+const Card = ({ first }) => {
+  // const handleClick = (response) => {};
+  // response();
+  const {
+    cell,
+    dob,
+    email,
+    gender,
+    location,
+    name,
+    picture,
+    phone,
+    nat,
+    registered,
+  } = first;
+
+  return (
+    <div className="container">
+      <div className="card mb-3" style={{ maxWidth: 540 }}>
+        <div className="row g-0">
+          <div className=""></div>
+          <div className="">
+            <div className="card-body p-5">
+              <div className="d-flex justify-content-between mt-2">
+                <img
+                  src={picture.medium}
+                  className="img-fluid rounded-start"
+                  alt="..."
+                />
+                <h5 className="card-title">
+                  {name.title} {name.first} {name.last}
+                </h5>
+              </div>
+
+              <div className="d-flex justify-content-between mt-2">
+                <img src={emailsvg} alt="" style={{ width: "30px" }} />
+                <p>{email}</p>
+              </div>
+              <div className="d-flex justify-content-between mt-2">
+                <img src={phonesvg} alt="" style={{ width: "30px" }} />
+                <p>{cell}</p>
+              </div>
+              <div className="d-flex justify-content-between mt-2">
+                <img src={locationsvg} alt="" style={{ width: "30px" }} />
+                <p>
+                  {location.city}-{location.country}
+                </p>
+              </div>
+
+              <p className="card-text text-end mt-2">Age : {dob.age}</p>
+              <p className="card-text text-end mt-2">
+                <small className="text-muted">
+                  Register Date : {registered.date.slice(0, 10)}
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
